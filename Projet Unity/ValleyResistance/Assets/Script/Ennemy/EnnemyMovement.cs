1df93d6dad5c02f12bedcee351c5b50264d83441
blikxem <kevin.kouoi@edu.esiee.fr>
2017-01-06 16:52:01 +0100

    version 2
    spawn + deplacement + animation

---
+++ Projet Unity/ValleyResistance/Assets/Script/Ennemy/EnnemyMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnnemyMovement : MonoBehaviour {
+    Transform player;
+    NavMeshAgent nav;
+
+    void Awake ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent <NavMeshAgent> ();
+    }
+
+
+    void Update ()
+    {
+        nav.SetDestination(player.position);
+    }
+}
---
+++ Projet Unity/ValleyResistance/Assets/Script/Manager/EnnemyManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnnemyManager : MonoBehaviour
+{
+    public GameObject[] ennemy;     // liste d'ennemies à spawn
+    public float spawnTime = 3f;    //temps entre chaque spawn
+    public Transform[] spawnPoints; //point de spawn à définir sur la carte
+
+
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn()
+    {
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length); // choisi un point de spawn aleatoire
+        int ennemyIndex = Random.Range(0, ennemy.Length);       // choisi un ennemy aleatoire
+
+        Instantiate(ennemy[ennemyIndex], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);// créer un ennemy
+    }
+
+
+}
